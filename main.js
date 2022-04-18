ebb17df07a18a03c81312ebe634bb81fdae4a16f
Gaston Ruiz <gastonruizojeda@gmail.com>
2022-04-18 18:45:46 -0300

    se terminaron los dos ejemploes en js

---
+++ main.js
@@ -0,0 +1,17 @@
+/* let nombre 
+
+while(nombre !== "esc"){
+   
+    nombre = prompt("Ingrese su nombre")
+    console.log("Su nombre es " + nombre); 
+    
+} */
+
+let numero
+numero = Number(prompt("Ingrese un numero"))
+console.log("su numero es " + numero)
+for(let x=0; x<= 10 ; x++) {
+    console.log(numero + x)
+}
+
+
